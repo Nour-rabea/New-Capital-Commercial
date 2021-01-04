024f72d584642b3531ba1dd977dd50a0bf7c0f9a
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2021-01-04 16:14:11 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+- Leaflet Map https://nour-rabea.github.io/New-Administrative-Capital/
+- Google Sheets template https://docs.google.com/spreadsheets/d/17FtwGOORkFA5hOplobtlBHmK8hQvAJEhCthb-sci3dQ/edit#gid=0
---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/17FtwGOORkFA5hOplobtlBHmK8hQvAJEhCthb-sci3dQ/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBRBVqGWH7Ibcr30SpLU-J49N9BCg_6uqE';
